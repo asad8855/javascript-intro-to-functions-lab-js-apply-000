e31fa273ec4f160e9fe17f1bae29903ce634860e
Arash Asady <aasady88@gmail.com>
2017-11-22 06:24:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,13 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string){
   var uppercase = "HELLO"
- uppercase.toUpperCase() === uppercase
- return "I can't hear you!" {
-   var lowercase = "hello"
+  if (uppercase.toUpperCase() === uppercase) {
+    return "I can't hear you!"
+}
+  var lowercase = "hello"
+ 
+  {
+   
    lowercase.toLowerCase() ===lowercase
    return "YES INDEED!"
  }
