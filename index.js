eb2b737ab3758527612a4a30671589085e690016
Arash Asady <aasady88@gmail.com>
2017-11-22 06:15:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string){
   var uppercase = "HELLO!"
-  var lowercase = "hello!"
-  if uppercase.toUpperCase === uppercase
+ uppercase.toUpperCase() === uppercase
+ return
   
 }
